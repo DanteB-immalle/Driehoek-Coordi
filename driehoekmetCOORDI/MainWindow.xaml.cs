1ecde56577359837b23342bb42367b298534d6db
Dante Budin <dante.budin@immalle.eu>
2017-12-01 10:57:17 +0100

    in gitter

--- driehoekmetCOORDI/MainWindow.xaml.cs
+++ driehoekmetCOORDI/MainWindow.xaml.cs
@@ -15,14 +15,45 @@ using System.Windows.Shapes;
 
 namespace driehoekmetCOORDI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+ 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void MijnKnop_Click(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush brush = new SolidColorBrush(Colors.Orange);
+
+            string s1 = TX1.Text;
+            string s2 = TY1.Text;
+
+            int xPlace2 = Convert.ToInt32(s1);
+            int yPlace2 = Convert.ToInt32(s2);
+
+            DrawTriangle(canvas, brush, xPlace2, yPlace2, 100, 150);
+
+            
+
+
+        }
+        private void DrawTriangle(Canvas drawingArea, SolidColorBrush brushToUse, double xPlace, double yPlace, double width, double height)
+        {
+            DrawLine(drawingArea, brushToUse, xPlace, yPlace, xPlace, yPlace + height);
+            DrawLine(drawingArea, brushToUse, xPlace, yPlace + height, xPlace + width, yPlace + height);
+            DrawLine(drawingArea, brushToUse, xPlace, yPlace, xPlace + width, yPlace + height);
+        }
+        private void DrawLine(Canvas drawingArea, SolidColorBrush brushToUse, double startX, double startY, double endX, double endY)
+        {
+            Line l = new Line();
+            l.X1 = startX; l.X2 = endX;
+            l.Y1 = startY; l.Y2 = endY;
+            l.Stroke = brushToUse;
+            drawingArea.Children.Add(l);
+        }
+
+      
     }
 }
